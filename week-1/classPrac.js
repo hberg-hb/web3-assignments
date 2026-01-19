7cf6cf89b651da1dcbb97c5da5cf6d9e1bb0dfbb
Pawan <aiinnoeng@gmail.com>
2026-01-19 15:00:43 +0530

    lec prac

---
+++ week-1/classPrac.js
@@ -0,0 +1,7 @@
+// example on how to generate hash of SHA256
+
+const crypto = require('crypto');
+const input = 'pk6862';
+const hash = crypto.createHash('sha256').update(input).digest('hex');
+console.log(hash);
+
